75d1950a62dafcd4e7468b1fc032bfc9e4a70f19
baskara restu <baskararw10@gmail.com>
2023-06-06 19:54:24 +0700

    FPP-13
    deleting unused reducer

--- projects/client/src/features/users/userSlice.js
+++ projects/client/src/features/users/userSlice.js
@@ -9,7 +9,6 @@ export const userSlice = createSlice({
   initialState: {
     user: null,
     isLoading: false,
-    lastVisitedPage: "/",
   },
   reducers: {
     setUser: (state, action) => {
@@ -19,13 +18,10 @@ export const userSlice = createSlice({
     setIsLoading: (state, action) => {
       state.isLoading = action.payload;
     },
-    setLastVisitedPage: (state, action) => {
-      state.lastVisitedPage = action.payload;
-    },
   },
 });
 
-export const { setIsLoading, setUser, setLastVisitedPage } = userSlice.actions;
+export const { setIsLoading, setUser } = userSlice.actions;
 
 export default userSlice.reducer;
 
@@ -40,11 +36,6 @@ export function registerUser(data, callback) {
 
       dispatch(setUser(response.data.data));
       console.log(response);
-      // Set the last visited page in the Redux store
-      const storedLastVisitedPage = localStorage.getItem("lastVisitedPage");
-      const lastVisitedPage = storedLastVisitedPage || "/";
-      dispatch(setLastVisitedPage(lastVisitedPage));
-
       // Call the callback function to navigate to /verification
       if (typeof callback === "function") {
         callback();
